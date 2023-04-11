0d5dce75301e153bdf85b33c7ce002df61925866
Mutagen345 <max06112003@gmail.com>
2023-04-11 09:41:20 +0300

    Сделал ЛР4

---
+++ 4/ZADACHA1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введите число х: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            double y1 = (Math.Sin(x) / x - 5) + Math.Pow(x, 3);
+            double y2 = Math.Log10(x) - (Math.Cos(x) / 3 * x + 6);
+            Console.WriteLine($"Результат 1 примера: {y1}");
+            Console.WriteLine($"Результат 2 - ого примера: {y2}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Не верное введённое значение!!!");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Деление на ноль");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ 4/ZADACHA2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введите число х: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            double f;
+            if ((x > -3) && (x < 3))
+            {
+                f = 2 / x;
+                Console.WriteLine($"f(x) = {f}");
+            }
+            else if (x <= -3)
+            {
+                f = 5 * x;
+                Console.WriteLine($"f(x) = {f}");
+            }
+            else
+            {
+                throw new Exception("Ошибка выход из диапазона!!!");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка недопустимое значение!!!");
+        }
+        Console.ReadLine();
+    }
+}
+
+
