e035561b00ca6399c621735a036bed3770afac3a
Mutagen345 <max06112003@gmail.com>
2023-04-17 09:06:21 +0300

    Сделал 8

---
+++ 8/ZADACHA1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZADACHA
+{
+    public struct Aeroflot
+    {
+        public string name;
+        public int nomer;
+        public string tip;
+
+
+        public Aeroflot(string n)
+        {
+            name = n;
+            nomer = 0;
+            tip = n;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\n\tНазвание пункта назначения - ");
+            sb.Append(this.name);
+            sb.Append("\n\tНомер рейса - ");
+            sb.Append(this.nomer);
+            sb.Append("\n\tТип самолета - ");
+            sb.Append(this.tip);
+            return sb.ToString();
+
+        }
+        class Program
+        {
+            const int a = 7;
+
+            static void Main(string[] args)
+            {
+
+                Aeroflot[] rais = new Aeroflot[a];
+
+                for (int i = 0; i < a;)
+                {
+                    try
+                    {
+                        Aeroflot fligt = new Aeroflot();
+                        Console.WriteLine("Введите название пункта назначения:");
+                        fligt.name = Console.ReadLine();
+                        Console.WriteLine("Введите номер рейса:");
+                        fligt.nomer = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите тип самолета:");
+                        fligt.tip = Console.ReadLine();
+                        rais[i] = fligt;
+                        i++;
+                        Console.WriteLine(fligt);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Ошибка: введено неправильное значение!");
+                    }
+
+                }
+                Console.WriteLine("\nСписок всех рейсов:");
+                IEnumerable<Aeroflot> query = rais.OrderBy(st => st.nomer);
+                foreach (Aeroflot st in query)
+                {
+                    Console.WriteLine(st.ToString());
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
---
+++ 8/ZADACHA2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Task_2
+{
+    public enum Post
+    {
+        Gardener = 150,
+        Loader = 121,
+        Uchitel = 150,
+        Agronomist = 11,
+        Programmer = 540,
+        Builder = 7345,
+        Analyst = 362,
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            foreach (string post in Enum.GetNames(typeof(Post)))
+            {
+                Console.Write($"Сколько времени проработал работник {post}: ");
+                int hours = int.Parse(Console.ReadLine());
+                Enum.TryParse<Post>(post, true, out Post postEnum);
+                Console.WriteLine($"{post} премия: {GetBonusLoc(postEnum, hours)}");
+                Console.WriteLine();
+            }
+        }
+        public static string GetBonusLoc(Post worker, int hours)
+        {
+            Accauntant acc = new Accauntant();
+            if (acc.AskForBonus(worker, hours))
+            {
+                return "Положена премия";
+            }
+            else
+            {
+                return "Не положена премия";
+            }
+        }
+    }
+    public class Accauntant
+    {
+        public Accauntant()
+        {
+        }
+        public bool AskForBonus(Post worker, int hours)
+        {
+            if ((int)worker < hours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
