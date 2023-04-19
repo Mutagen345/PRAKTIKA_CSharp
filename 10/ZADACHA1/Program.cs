5605729087f4c4617fe61115c577c3646d166431
Mutagen345 <max06112003@gmail.com>
2023-04-19 19:28:06 +0300

    Сделал 10

---
+++ 10/ZADACHA1/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace ProgramSpaceTwo
+{
+    public enum Post
+    {
+        Accountant = 164,
+        Engineer = 180,
+        Electrician = 150,
+        Mechanic = 154,
+        Agronomist = 170,
+        Programmer = 160,
+        Builder = 100
+    }
+
+    public class Accauntant
+    {
+        private int hours;
+        // конструктор
+        public Accauntant()
+        {
+            hours = 0;
+        }
+        public void Foreaching()
+        {
+            foreach (string post in Enum.GetNames(typeof(Post)))
+            {
+                Console.Write($"Введите, cколько времени проработал за этот месяц {post}: ");
+                hours = Convert.ToInt32(Console.ReadLine());
+                Enum.TryParse<Post>(post, true, out Post postEnum);
+                Console.WriteLine($"Полагается ли {post} премия: {AskForBonus(postEnum, hours)}");
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Решени, начислять бонус или нет
+        /// </summary>
+        /// <param name="worker"> Перечисление профессий </param>
+        /// <param name="hours"> Время работы работника </param>
+        /// <returns></returns>
+        public bool AskForBonus(Post worker, int hours)
+        {
+            if ((int)worker < hours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    public class Legacy : Accauntant
+    {
+        private int d;
+        // конструктор
+        public Legacy()
+        {
+            d = 0;
+
+        }
+        public int Legacy_Result()
+        {
+            int a = 12;
+            int b = 0;
+            int oneYers = 0;
+            int[] enums = (int[])Enum.GetValues(typeof(Post));
+            int sizeEnum = Enum.GetNames(typeof(Post)).Length;
+            for (int i = 0; i < sizeEnum; i++)
+            {
+                d = enums[i];
+                oneYers += a * (b + d);
+            }
+            return oneYers;
+
+        }
+    }
+
+    public class ProgramMain
+    {
+        public static void Main()
+        {
+            Accauntant acc = new Accauntant();
+            acc.Foreaching();
+
+            Legacy l = new Legacy();
+            Console.WriteLine($"{l.Legacy_Result()} Часов в год на работников.");
+        }
+    }
+}
---
+++ 10/ZADACHA2/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Animal
+{
+    public int K { get; protected set; }
+    public string Alias { get; private set; }
+    public double Height { get; private set; }
+    public Animal(string alias, double height, int k) { Alias = alias; Height = height; K = k; }
+    public double GetWeight() => K * Height;
+    public override string ToString() => $"Кличка: '{Alias}'; Рост: {Height}м., Вес: {GetWeight()}кг.";
+}
+public class Cat : Animal
+{
+    public string Breed { get; private set; }
+    public Cat(string breed, string alias, double height) : base(alias, height, 15) => Breed = breed;
+    public double GetWeightInGrams() => GetWeight() * 1000;
+    public override string ToString() => $"Кошка. Порода: '{Breed}'; {base.ToString()}; Вес в граммах: {GetWeightInGrams()}г.";
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new Animal("Некто", 0.1, 13) + "\n" + new Cat("Шерстяная", "СобакаСутулая", 0.3));
+        Console.ReadLine();
+    }
+}
