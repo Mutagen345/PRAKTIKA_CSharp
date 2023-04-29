3ac32874de237393e7e171b9aacb8d7a21dcf83f
Mutagen345 <max06112003@gmail.com>
2023-04-29 17:16:40 +0300

    Сделал 17

--- 17/ZADACHA1/Program.cs
+++ 17/ZADACHA1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GeometryLibrary;
+
+// создание объекта треугольника
+Triangle triangle = new Triangle(3, 4, 5);
+
+
+    // вычисление периметра иругольника
+    double Perimeter = triangle.Perimeter();
+
+    // вычисление площади треугольника
+    double Area = triangle.Area();
+
+    // определение типа треугольника
+    string type = triangle.Type();
+
+
+// создание объекта прямоугольника
+Rectangle rectangle = new Rectangle(2, 3);
+
+// вычисление периметра прямоугольника
+double perimeter = rectangle.Perimeter();
+
+// вычисление площади прямоугольника
+double area = rectangle.Area();
+
+Console.WriteLine(Perimeter);
+Console.WriteLine(Area);
+Console.WriteLine(type);
+Console.WriteLine(perimeter);
+Console.WriteLine(area);
