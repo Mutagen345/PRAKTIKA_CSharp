1d9a9b9abae3210347d756dd3ff15f71921bd453
Mutagen345 <max06112003@gmail.com>
2023-05-04 08:51:59 +0300

    Сделал 21

---
+++ 21/ZADACHA1/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        (new Thread(() =>
+        {
+            Computer comp2 = new Computer();
+            comp2.OS = OS.getInstance("Windows 10");
+            Console.WriteLine(comp2.OS.Name);
+
+        })).Start();
+
+        Computer comp = new Computer();
+        comp.Launch("Windows 8.1");
+        Console.WriteLine(comp.OS.Name);
+        Console.ReadLine();
+    }
+    class Computer
+    {
+        public OS OS { get; set; }
+        public void Launch(string osName)
+        {
+            OS = OS.getInstance(osName);
+        }
+    }
+    class OS
+    {
+        private static OS instance;
+
+        public string Name { get; private set; }
+
+        protected OS(string name)
+        {
+            this.Name = name;
+        }
+
+        public static OS getInstance(string name)
+        {
+            if (instance == null)
+                instance = new OS(name);
+            return instance;
+        }
+    }
+}
